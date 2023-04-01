b65488a7dd5ac221040c22d0d1d580d36617ce5a
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-04-01 12:10:20 +0100

    Update Day-10_Binary_Numbers.cpp

--- Day-10_Binary_Numbers.cpp
+++ Day-10_Binary_Numbers.cpp
@@ -39,3 +39,67 @@ string rtrim(const string &str) {
     return s;
 }
 
+/*
+
+Objective
+Today, we're working with binary numbers. Check out the Tutorial tab for learning materials and an instructional video!
+
+Task
+Given a base- integer, , convert it to binary (base-). Then find and print the base- integer denoting the maximum number of consecutive 's in 's binary representation. When working with different bases, it is common to show the base as a subscript.
+
+Example
+
+The binary representation of  is . In base , there are  and  consecutive ones in two groups. Print the maximum, .
+
+Input Format
+
+A single integer, .
+
+Constraints
+
+Output Format
+
+Print a single base- integer that denotes the maximum number of consecutive 's in the binary representation of .
+
+Sample Input 1
+
+5
+Sample Output 1
+
+1
+Sample Input 2
+
+13
+Sample Output 2
+
+2
+Explanation
+
+Sample Case 1:
+The binary representation of  is , so the maximum number of consecutive 's is .
+
+Sample Case 2:
+The binary representation of  is , so the maximum number of consecutive 's is .
+
+Language
+C++
+More
+26272829303132333435363738394041
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+
+
+int main()
+…}
+
+Line: 41 Col: 1
+
+Test against custom input
+Blog
+
+*/
